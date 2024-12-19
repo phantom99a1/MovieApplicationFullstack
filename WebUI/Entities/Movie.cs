9fc1624498f7ed1304e6e4557a41bd3afa83df78
phantom99a1 <viendanho99a1@gmail.com>
2024-12-20 04:14:15 +0700

    Add new project

---
+++ WebUI/Entities/Movie.cs
@@ -0,0 +1,21 @@
+﻿namespace WebUI.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Language { get; set; } = string.Empty;
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string CoverImage { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
